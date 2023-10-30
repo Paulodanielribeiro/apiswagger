b4bf930f37fabba2bc4c2661695544f84797b632
Paulo Daniel Moraes Ribeiro <paulomoraesr6@gmail.com>
2023-10-30 12:50:22 -0400

    add

--- src/routes/Product.ts
+++ src/routes/Product.ts
@@ -1,40 +1,25 @@
-import { Router, Request, Response } from 'express'
-import { Severity } from '../enums/Severity'
-import { ProductController } from '../controllers/ProductController'
-import { Product } from '../entity/Product'
+import { Router, Request, Response } from 'express';
+import { ProductController } from '../controllers/ProductController';
+import { Product } from '../entity/Product';
 
-export const productsRouter = Router()
-const productCtrl = new ProductController()
+export const productsRouter = Router();
+const productCtrl = new ProductController();
 
 productsRouter.post('/', async (req: Request, res: Response) => {
-  const { description, deadline, severity } = req.body
+  const { description } = req.body;
 
-  const errorMessages: string[] = []
+  const errorMessages: string[] = [];
 
   if (!description) {
-    errorMessages.push('Description cannot be empty')
-  }
-
-  const deadlineDate = new Date(deadline)
-  if (deadlineDate < new Date()) {
-    errorMessages.push('Deadline cannot be a past date')
-  }
-
-  const severityNumber = Number(severity)
-  if (
-    isNaN(severityNumber) ||
-    severityNumber < Severity.LOW ||
-    severityNumber > Severity.HIGH
-  ) {
-    errorMessages.push('Invalid severity level')
+    errorMessages.push('Description cannot be empty');
   }
 
   if (errorMessages.length === 0) {
-    const product = new Product()
-    product.description = description
-    const savedProduct = await productCtrl.save(product)
-    return res.status(201).json({ product: savedProduct })
+    const product = new Product();
+    product.description = description;
+    const savedProduct = await productCtrl.save(product);
+    return res.status(201).json({ product: savedProduct });
   }
 
-  return res.status(400).json({ errorMessages })
-})
+  return res.status(400).json({ errorMessages });
+});
